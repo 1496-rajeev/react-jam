2f98ea9d53b2a43eb7ad3d8ce97c66db905f8fc0
1496-rajeev <1496piku@gmail.com>
2021-03-12 15:32:37 +0530

    added props nad state

---
+++ myapp/src/Person/Person.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const person = (props) => {
+    return (
+         <div>
+             <h1>hello I am {props.name} and my age is {props.age}</h1>
+             <p>{props.children}</p>
+         </div>
+    )
+};
+
+export default person;

4856cc2c4360207be94e32e7a8647f754ef98a68
1496-rajeev <1496piku@gmail.com>
2021-03-13 02:06:56 +0530

    added hook useState()

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,22 +1,32 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Person from "./Person/Person";
 import './App.css';
 
-class App extends Component {
-   state = {
-     persons: [
-       {name:"max", age: 28},
-       {name:"rajeev", age:30}
-     ]
+const  App = props => {
+
+  const [personState, setPersonState] =  useState({
+    persons: [
+      {name:"max", age: 28},
+      {name:"rajeev", age:30}
+    ]
+  });
+ 
+  const [otherState, setOtherState] = useState("other usestate")
+  console.log(personState, otherState)
+
+  const switchHandler = () => {
+    setPersonState({persons: [
+      {name:"maxima", age: 28},
+      {name:"rajeeva", age:30}
+    ]})
    }
 
-  render() {
     return (
       <div className="App">
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>children test</Person>
+        <Person name={personState.persons[0].name} age={personState.persons[0].age}>children test</Person>
+        <button onClick={switchHandler}>click me</button>
       </div>
     );
-  }
 }
 
 export default App;
